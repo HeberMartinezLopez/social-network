831a60dd58bc2d2c3063fae2432ebaf09a44a54a
Heber Martinez Lopez <hebermartinez14@gmail.com>
2023-11-27 10:45:15 -0800

    start userController

---
+++ controllers/userController.js
@@ -0,0 +1,53 @@
+const { User, Thought } = require('../models');
+const userController = {
+    async getAllUsers(req, res){
+        try {
+            
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    },
+    async getOneUser(req, res){
+        try {
+            
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    },
+    async createUser(req, res){
+        try {
+            
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    },
+    async updateUser(req, res){
+        try {
+            
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    },
+    async deleteUser(req, res){
+        try {
+            
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    },
+    async addFriend(req, res){
+        try {
+            
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    },
+    async removeFriend(req, res){
+        try {
+            
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    }
+}
+module.exports = userController
