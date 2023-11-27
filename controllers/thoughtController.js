b9cf961f8d549a18f14197962c5fd9ee1be0c5e4
Heber Martinez Lopez <hebermartinez14@gmail.com>
2023-11-27 11:24:07 -0800

    add thought routes

---
+++ controllers/thoughtController.js
@@ -0,0 +1,54 @@
+const { User, Thought } = require('../models');
+const thoughtController = {
+    async getAllThoughts(req, res){
+        try {
+            
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    },
+    async getOneThought(req, res){
+        try {
+            
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    },
+    async createThought(req, res){
+        try {
+            
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    },
+    async updateThought(req, res){
+        try {
+            
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    },
+    async deleteThought(req, res){
+        try {
+            
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    },
+    async addReaction(req, res){
+        try {
+            
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    },
+    async removeReaction(req, res){
+        try {
+            
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    },
+}
+
+module.exports = thoughtController;
